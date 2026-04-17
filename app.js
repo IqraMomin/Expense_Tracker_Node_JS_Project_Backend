c21cac00e2beb1eaf0e87eb0d8f3002d8ffe302e
IqraMomin <iqra2018momin@gmail.com>
2026-04-17 20:06:40 +0530

    signUp

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const app = express();
 const port = 4000;
 const userRoutes = require("./routes/userRoutes");
 const userModel = require("./models/users");
+const db = require("./utils/db-conection");
+
+app.use(express.json());
 
 app.use("/users",userRoutes);
 
-app.listen(port,()=>{
-    console.log(`Server is Up and Running on port http://localhost:${port}`)
+db.sync({force:false}).then(()=>{
+    app.listen(port,()=>{
+        console.log(`Server is Up and Running on port http://localhost:${port}`)
+    })
 })
--- controller/usersController.js
+++ controller/usersController.js
@@ -1,5 +1,26 @@
+const Users = require("../models/users");
+
 const addUser = async(req,res)=>{
-    console.log("Adding user");
+    try{
+        const existingUser = await Users.findOne({
+            where:{
+                email:req.body.email
+            }
+        })
+        if(existingUser){
+            return res.status(400).json({message:"Email Already exists"})
+        }
+        const user = await Users.create(req.body);
+        return res.status(201).json(user);
+    }
+    catch(err){
+        if(err.name==="SequelizeUniqueConstraintError"){
+            return res.status(400).json({message:"Email Already Exists"});
+        }
+        res.status(500).json({message:"Something went wrong"});
+    }
+   
+
 }
 
 module.exports = {addUser}
