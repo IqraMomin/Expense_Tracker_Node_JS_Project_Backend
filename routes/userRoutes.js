ab80fde24eb0abb381e8f13bf6646b6dcf1cdc60
IqraMomin <iqra2018momin@gmail.com>
2026-04-22 00:16:59 +0530

    forgot password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const usersController = require("../controller/usersController");
 router.post("/signup",usersController.addUser);
 router.post("/login",usersController.loginUser);
 router.get("/premium/showLeaderBoard",usersController.getLeaderBoard);
-router.post("/password/forgotpassword")
+router.post("/password/forgotpassword",usersController.resetPassword);
 
 module.exports = router;
 
