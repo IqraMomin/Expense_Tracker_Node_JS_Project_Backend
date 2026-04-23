ede76f4fe4caa0a245296d2027d0efee02988a19
IqraMomin <iqra2018momin@gmail.com>
2026-04-24 04:29:12 +0530

    Deployment 2

--- app.js
+++ app.js
@@ -12,18 +12,23 @@ const passwordRoutes = require("./routes/passwordRoutes");
 require("./models");
 const fs = require("fs");
 const morgan = require("morgan");
+const path = require("path");
+const helmet = require("helmet")
 
 
 const accessLog = fs.createWriteStream(path.join(__dirname,"access.log"),{flags:"a"});
 app.use(express.json());
 app.use(cors());
+app.use(morgan('combined',{stream:accessLog}));
+app.use(helmet());
+
 
 app.use("/pay",paymentRoutes)
 app.use("/users",userRoutes);
 app.use("/expenses",expenseRoutes);
 app.use("/gemini",geminiRoutes);
 app.use("/password",passwordRoutes);
-app.use(morgan('combined',{stream:accessLog}));
+
 
 db.sync({force:false}).then(()=>{
     app.listen(process.env.PORT||3000,()=>{
