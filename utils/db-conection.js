edc50b886336eed66ac736f80b68f946586d8e9a
IqraMomin <iqra2018momin@gmail.com>
2026-05-19 03:31:17 +0530

    Added Changes

--- utils/db-conection.js
+++ utils/db-conection.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const {Sequelize} = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_NAME,'root',process.env.DATABASE_PASSWORD,{
-    host:"localhost",dialect:"mysql"
+    host:process.env.DB_HOST,dialect:"mysql"
 });
 
 (async ()=>{
